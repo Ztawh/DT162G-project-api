10cde8b7f875057c947c34bd34ab4966a4e90233
Amanda Hwatz Björkholm <amandahwatz@gmail.com>
2022-01-03 17:21:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Javascriptbaserad Webbutveckling - Moment 3
+## API
+
+Uppgiften gick ut på att skapa en REST-webbtjänst med express med stöd för GET, GET med ID, DELETE och POST. Webbtjänsten ska sen konsumeras av en webbklient. 
+
+Webbtjänsten ska spara data om kurser i MongoDB.
+
+## Klona
+Klona repot med kommandot:
+* git clone https://github.com/Ztawh/DT162G-moment3-api.git
+
+Du behöver ha node.js installerat på din dator. Installera alla paket genom att skriva:
+* npm install
+i terminalen
+
+Därefter för att köra servern skriver du:
+* node app.js
+i terminalen
---
+++ app.js
@@ -0,0 +1,111 @@
+/* Skapat av Amanda Hwatz Björkholm 
+*/
+
+// MongoDb
+// Lösen: nichof-6vewpo-sixbaW
+// Anv: Admin
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/bloggDB', { useNewUrlParser: true });
+
+// Koppla upp till databas på Mlab
+// mongoose.connect("mongodb+srv://admin:nichof-6vewpo-sixbaW@cluster0.1oheg.mongodb.net/myCourses?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected!"));
+
+
+// Sätter att inkommande data är json-objekt
+app.use(express.json());
+//const port = process.env.PORT;
+const port = 3000;
+app.use(express.static(__dirname + '/posts'));
+app.listen(port, () => console.log(`Server started`));
+
+// Sätt headers
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+// Schema
+const courseSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    date: {type: Date, default: Date.now}
+});
+
+// Model
+const Post = mongoose.model('posts', courseSchema);
+
+// GET inlägg
+app.get('/posts', async (req, res) => {
+    try {
+        // Hämta alla inlägg
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (err){
+        res.status(500).json({message: err.message});
+    }
+});
+
+// GET med ID
+app.get("/posts/:id", async (req, res) => {
+    let id = req.params.id;
+  
+    try {
+        // Hämta kurs med specifikt _id
+        res.json(await Post.findById(id));
+    } catch {
+        res.json({message: "Couldn't find post"});
+    }
+});
+
+// DELETE
+app.delete("/posts/:id", async (req, res) => {
+    let id = req.params.id;
+
+    try {
+        // Ta bort kurs med visst _id
+        await Post.findByIdAndDelete(id);
+        res.json({message: "Post deleted"});
+    } catch {
+        res.json({message: "Couldn't find post"});
+    }
+});
+
+// POST
+app.post("/posts", (req, res) => {
+    try {
+        // Sätt inskickad data till model och spara till databasen
+        let newPost = new Post(req.body);
+        newPost.save();
+        res.json({message: "Post added"});
+    } catch {
+        res.json({message: "Couldn't add post"})
+    }
+});
+
+// PUT
+app.put("/posts/:id", async (req, res) => {
+    // Sätt id till det inlägg som ska uppdateras
+    // Sätt ny data
+    let id = req.params.id;
+    const filter = {_id: id};
+    const updatePost = req.body;
+    
+
+    try {
+        // Redigera kurs med visst _id
+        await Post.findByIdAndUpdate(filter, updatePost);
+        res.json({message: "Post updated"});
+    } catch {
+        res.json({message: "Couldn't find post"});
+    }
+});
